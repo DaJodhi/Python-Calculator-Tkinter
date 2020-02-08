87c67cf9fdf00c8647117148e0db9a014c66b4c8
Jodhviir Sekhon <49446509+DaJodhi@users.noreply.github.com>
2020-02-08 09:21:24 +0000

    Update 2.1
    The program has been updated to use OOP, and also to comply with PEP 8 standards, with a rating of 9.89 out of 10 on Pylint. This means that the code is better documented and more efficient.

--- calculator.py
+++ calculator.py
@@ -1,137 +1,158 @@
-from tkinter import *
-import tkinter.messagebox as box
+"""A simple calculator made in Tkinter"""
 from functools import partial
+from tkinter import Label, Tk, Button, Frame, messagebox
 
-# Version 2, The Speed and Docs Update
+# Version 2.1, The Speed and Docs Update
 
-root = Tk()
+ROOT = Tk()
 
-calculation = ""
-root.geometry("400x450")
-root.title("Calculator 2.0, by Jodhi")
-root.resizable(False, False)
-root.config(bg="black")
-
-# This widget will show the calculation
-display = Label(root, width=57, height=5)
+ROOT.geometry("400x450")
+ROOT.title("Calculator 2.0, by Jodhi")
+ROOT.resizable(False, False)
+ROOT.config(bg="black")
 
 
-# Updates the display every time the calculation  is done
-def update():
-    global calculation
-    display.configure(text=calculation)
+DISPLAY = Label(ROOT, width=57, height=5)
 
 
-# template command for simple buttons
-def add_char(x):
-    global calculation
-    calculation += x
-    update()
-
-
-def brackets():
-    global calculation
-    if calculation.count("(") == calculation.count(")") + 1:
-        calculation += ")"
-    else:
-        calculation += "("
-    update()
-
-
-# Removes the last character from the display
-def backspace():
-    global calculation
-    calculation = calculation[:-1]
-    update()
-
-
-# Simply clears the calculator display
-def clear():
-    global calculation
+class Calculator:
+    """A class is used so that we do not have to use a global statement in each function"""
     calculation = ""
-    update()
+
+    @classmethod
+    def update(cls):
+        """Updates the calculator display"""
+        DISPLAY.configure(text=cls.calculation)
+
+    @classmethod
+    def add_char(cls, char):
+        """Template command for simple buttons"""
+        cls.calculation += char
+        cls.update()
+
+    @classmethod
+    def brackets(cls):
+        """Adds brackets"""
+        if cls.calculation.count("(") == cls.calculation.count(")") + 1:
+            cls.calculation += ")"
+        else:
+            cls.calculation += "("
+        cls.update()
+
+    @classmethod
+    def backspace(cls):
+        """Removes the last character from the display"""
+        cls.calculation = cls.calculation[:-1]
+        cls.update()
+
+    @classmethod
+    def clear(cls):
+        """Clears the calculator display"""
+        cls.calculation = ""
+        cls.update()
+
+    @classmethod
+    def equal(cls):
+        """Evaluates the answer to the calculation
+        If the answer is larger than 57 digits, it is saved to a results file
+        Any trailing .0 is removed from simple divisions"""
+        try:
+            cls.calculation = str(eval(cls.calculation))
+            if cls.calculation.endswith(".0"):
+                cls.calculation = cls.calculation[:-2]
+            if len(cls.calculation) > 57:
+                file = open("result.txt", "a+")
+                file.write(cls.calculation + "\n \n")
+                cls.clear()
+                messagebox.showinfo("Saved Result", "The result was saved in 'result.txt' and the"
+                                                    " calculator display was cleared.")
+                file.close()
+            cls.update()
+        except ZeroDivisionError:
+            messagebox.showerror("Zero Division Error", "Cookie monster.")
+        except SyntaxError:
+            messagebox.showerror("Error", "You did something wrong.")
+        except OverflowError:
+            messagebox.showerror("Overflow Error", "That's too small man.")
+        except TypeError:
+            messagebox.showerror("Type Error", "We don't do algebra here.")
+
+    @classmethod
+    def dot(cls):
+        """Adds a decimal point"""
+        if cls.calculation[:-1] != ".":
+            cls.calculation += "."
+            cls.update()
 
 
-def equal():
-    global calculation
-    try:
-        calculation = str(eval(calculation))
-        if calculation.endswith(".0"):
-            calculation = calculation[:-2]
-        # if the result is too big for the calculator display, it saves the result in a text file and clears the calculator display
-        if len(calculation) > 57:
-            file = open("result.txt", "a+")
-            file.write(calculation + "\n \n")
-            clear()
-            box.showinfo("Saved Result", "The result of your calculation was saved in 'result.txt' and the calculator display was cleared.")
-            file.close()
-        update()
-    except ZeroDivisionError:
-        box.showerror("Zero Division Error", "Cookie monster.")
-    except SyntaxError:
-        box.showerror("Error", "You did something wrong.")
-    except OverflowError:
-        box.showerror("Overflow Error", "That's too small man.")
-    # This error was encountered when doing something like this: 2(3+4)
-    except TypeError:
-        box.showerror("Type Error", "We don't do algebra here.")
-
-
-def dot():
-    global calculation
-    if calculation[:-1] != ".":
-        calculation += "."
-        update()
-
-
-buttonFrame = Frame(root)
+BUTTON_FRAME = Frame(ROOT)
 
 # The following buttons can be held down
-num1 = Button(buttonFrame, text="1", command=partial(add_char, "1"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-num2 = Button(buttonFrame, text="2", command=partial(add_char, "2"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-num3 = Button(buttonFrame, text="3", command=partial(add_char, "3"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-num4 = Button(buttonFrame, text="4", command=partial(add_char, "4"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-num5 = Button(buttonFrame, text="5", command=partial(add_char, "5"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-num6 = Button(buttonFrame, text="6", command=partial(add_char, "6"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-num7 = Button(buttonFrame, text="7", command=partial(add_char, "7"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-num8 = Button(buttonFrame, text="8", command=partial(add_char, "8"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-num9 = Button(buttonFrame, text="9", command=partial(add_char, "9"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-num0 = Button(buttonFrame, text="0", command=partial(add_char, "0"), fg="white", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
-back = Button(buttonFrame, text=" <-", command=backspace, fg="black", bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM1 = Button(BUTTON_FRAME, text="1", command=partial(Calculator.add_char, "1"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM2 = Button(BUTTON_FRAME, text="2", command=partial(Calculator.add_char, "2"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM3 = Button(BUTTON_FRAME, text="3", command=partial(Calculator.add_char, "3"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM4 = Button(BUTTON_FRAME, text="4", command=partial(Calculator.add_char, "4"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM5 = Button(BUTTON_FRAME, text="5", command=partial(Calculator.add_char, "5"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM6 = Button(BUTTON_FRAME, text="6", command=partial(Calculator.add_char, "6"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM7 = Button(BUTTON_FRAME, text="7", command=partial(Calculator.add_char, "7"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM8 = Button(BUTTON_FRAME, text="8", command=partial(Calculator.add_char, "8"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM9 = Button(BUTTON_FRAME, text="9", command=partial(Calculator.add_char, "9"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+NUM0 = Button(BUTTON_FRAME, text="0", command=partial(Calculator.add_char, "0"), fg="white",
+              bg="gray", width=9, height=4, repeatdelay=100, repeatinterval=100)
+BACK = Button(BUTTON_FRAME, text=" <-", command=Calculator.backspace, fg="black", bg="gray",
+              width=9, height=4, repeatdelay=100, repeatinterval=100)
 
-add = Button(buttonFrame, text="+", command=partial(add_char, "+"), fg="black", bg="gray", width=9, height=4)
-minus = Button(buttonFrame, text="-", command=partial(add_char, "-"), fg="black", bg="gray", width=9, height=4)
-divide = Button(buttonFrame, text=" ÷ ", command=partial(add_char, "/"), fg="black", bg="gray", width=9, height=4)
-mult = Button(buttonFrame, text=" × ", command=partial(add_char, "*"), fg="black", bg="gray", width=9, height=4)
-equals = Button(buttonFrame, text=" = ", fg="black", bg="red", command=equal, width=9, height=4)
+ADD = Button(BUTTON_FRAME, text="+", command=partial(Calculator.add_char, "+"), fg="black",
+             bg="gray", width=9, height=4)
+MINUS = Button(BUTTON_FRAME, text="-", command=partial(Calculator.add_char, "-"), fg="black",
+               bg="gray", width=9, height=4)
+DIVIDE = Button(BUTTON_FRAME, text=" ÷ ", command=partial(Calculator.add_char, "/"), fg="black",
+                bg="gray", width=9, height=4)
+MULT = Button(BUTTON_FRAME, text=" × ", command=partial(Calculator.add_char, "*"), fg="black",
+              bg="gray", width=9, height=4)
+EQUALS = Button(BUTTON_FRAME, text=" = ", fg="black", bg="red", command=Calculator.equal,
+                width=9, height=4)
 
-xy_button = Button(buttonFrame, text="xª", command=partial(add_char, "**"), fg="black", bg="gray", width=9, height=4)
-brackets_button = Button(buttonFrame, text="()", command=brackets, width=9, height=4, fg="black", bg="gray")
-dot_button = Button(buttonFrame, text=".", command=dot, fg="white", bg="gray", width=9, height=4)
-clear_button = Button(buttonFrame, text="C", command=clear, fg="red", bg="gray", width=9, height=4)
+XY_BUTTON = Button(BUTTON_FRAME, text="xª", command=partial(Calculator.add_char, "**"),
+                   fg="black", bg="gray", width=9, height=4)
+BRACKETS_BUTTON = Button(BUTTON_FRAME, text="()", command=Calculator.brackets,
+                         width=9, height=4, fg="black", bg="gray")
+DOT_BUTTON = Button(BUTTON_FRAME, text=".", command=Calculator.dot, fg="white", bg="gray",
+                    width=9, height=4)
+CLEAR_BUTTON = Button(BUTTON_FRAME, text="C", command=Calculator.clear, fg="red", bg="gray",
+                      width=9, height=4)
 
-clear_button.grid(row=1, column=1)
-brackets_button.grid(row=1, column=2)
-xy_button.grid(row=1, column=3)
-divide.grid(row=1, column=4)
-num7.grid(row=2, column=1)
-num8.grid(row=2, column=2)
-num9.grid(row=2, column=3)
-mult.grid(row=2, column=4)
-num4.grid(row=3, column=1)
-num5.grid(row=3, column=2)
-num6.grid(row=3, column=3)
-minus.grid(row=3, column=4)
-num1.grid(row=4, column=1)
-num2.grid(row=4, column=2)
-num3.grid(row=4, column=3)
-add.grid(row=4, column=4)
-back.grid(row=5, column=1)
-num0.grid(row=5, column=2)
-dot_button.grid(row=5, column=3)
-equals.grid(row=5, column=4)
+CLEAR_BUTTON.grid(row=1, column=1)
+BRACKETS_BUTTON.grid(row=1, column=2)
+XY_BUTTON.grid(row=1, column=3)
+DIVIDE.grid(row=1, column=4)
+NUM7.grid(row=2, column=1)
+NUM8.grid(row=2, column=2)
+NUM9.grid(row=2, column=3)
+MULT.grid(row=2, column=4)
+NUM4.grid(row=3, column=1)
+NUM5.grid(row=3, column=2)
+NUM6.grid(row=3, column=3)
+MINUS.grid(row=3, column=4)
+NUM1.grid(row=4, column=1)
+NUM2.grid(row=4, column=2)
+NUM3.grid(row=4, column=3)
+ADD.grid(row=4, column=4)
+BACK.grid(row=5, column=1)
+NUM0.grid(row=5, column=2)
+DOT_BUTTON.grid(row=5, column=3)
+EQUALS.grid(row=5, column=4)
 
-display.pack()
-buttonFrame.pack()
+DISPLAY.pack()
+BUTTON_FRAME.pack()
 
-root.mainloop()
+ROOT.mainloop()
